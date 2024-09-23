9df52b4830ed3d47cbc8ab7bcc1770c9f4396f8d
Emiliano Acosta <emilianoacosta18@outlook.com>
2024-09-23 17:46:54 -0300

    Se modifican rutas e importaciones

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
 
 import Pedido from './components/PedidoFormV1/Pedido';
 import NavBar from './components/NavBarV1/NavBar';
@@ -8,25 +10,77 @@ import PedidosFinalizadosPage from './pages/PedidosFinalizadosPage';
 
 import './App.css'
 
+import { createHashRouter } from 'react-router-dom';
+
+// function App() {
+
+//   const [pedidos, setPedidos] = useState([]);
+//   const confirmarPedido = (pedido) => {
+//     setPedidos([...pedidos, pedido]);
+//   };
+
+//   const router = createHashRouter([
+//     {
+//       path: "/",
+//       element: <Pedido confirmarPedido={confirmarPedido} />,
+//     },
+//     {
+//       path: "/pedidos",
+//       element: <VerPedidoPage pedidos={pedidos} />,
+//     },
+//     {
+//       path: "/pedidosFinalizados",
+//       element: <PedidosFinalizadosPage pedidos={pedidos} />,
+//     }
+
+
+//   ])
+//   return (
+//     <>
+//       <BrowserRouter>
+//         <RouterProvider router={router}>
+//           <NavBar />
+
+//           {/* <Routes>
+//             <Route path="/" element={<Pedido confirmarPedido={confirmarPedido} />} />
+//             <Route path="/pedidos" element={<VerPedidoPage pedidos={pedidos} />} />
+//             <Route path="/pedidosFinalizados" element={<PedidosFinalizadosPage pedidos={pedidos} />} />
+
+//           </Routes> */}
+//         </RouterProvider>
+//       </BrowserRouter>
+//     </>
+//   );
+// }
+
+// export default App;
+
 function App() {
   const [pedidos, setPedidos] = useState([]);
+
   const confirmarPedido = (pedido) => {
     setPedidos([...pedidos, pedido]);
   };
 
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Pedido confirmarPedido={confirmarPedido} />,
+    },
+    {
+      path: '/pedidos',
+      element: <VerPedidoPage pedidos={pedidos} />,
+    },
+    {
+      path: '/pedidosFinalizados',
+      element: <PedidosFinalizadosPage pedidos={pedidos} />,
+    },
+  ]);
+
   return (
     <>
-    <BrowserRouter>
       <NavBar />
-      
-        <Routes>
-          <Route path="/" element={<Pedido confirmarPedido={confirmarPedido} />}/>
-          <Route path="/pedidos" element={<VerPedidoPage pedidos={pedidos} />}/>
-          <Route path="/pedidosFinalizados" element={<PedidosFinalizadosPage pedidos={pedidos} />}/>
-
-        </Routes>
-  
-      </BrowserRouter>
+      <RouterProvider router={router} />
     </>
   );
 }
