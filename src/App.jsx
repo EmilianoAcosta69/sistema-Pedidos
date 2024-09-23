24b354b681e5e9c944ec686670e5560d539a2417
Emiliano Acosta <emilianoacosta18@outlook.com>
2024-09-23 17:18:56 -0300

    solucion de errores

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Pedido from './components/PedidoFormV1/Pedido';
 import NavBar from './components/NavBarV1/NavBar';
 import VerPedidoPage from './pages/VerPedidoPage';
-import PedidosFinalizados from './pages/PedidosFinalizados';
+import PedidosFinalizadosPage from './pages/PedidosFinalizadosPage';
 
 import './App.css'
 
@@ -22,7 +22,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Pedido confirmarPedido={confirmarPedido} />}/>
           <Route path="/pedidos" element={<VerPedidoPage pedidos={pedidos} />}/>
-          <Route path="/pedidosFinalizados" element={<PedidosFinalizados pedidos={pedidos} />}/>
+          <Route path="/pedidosFinalizados" element={<PedidosFinalizadosPage pedidos={pedidos} />}/>
 
         </Routes>
   
---
+++ src/pages/PedidosFinalizadosPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import PedidosFinalizados from '../components/PedidoFormV1/PedidosFinalizados';
+
+const PedidosFinalizadosPage = () => {
+    return (
+        <PedidosFinalizados></PedidosFinalizados>
+    )
+}
+
+export default PedidosFinalizadosPage;
